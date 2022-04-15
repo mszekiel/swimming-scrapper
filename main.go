4fb438872c7c62651c8044f3843ce5b8395f92ee
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2022-04-15 13:25:35 +0200

    simple fetch

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"os"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"log"
+	"net/http"
 )
 
 func main() {
-	db, err := gorm.Open(postgres.Open(os.Getenv("PG_DSN")), &gorm.Config{})
+	client := http.Client{}
+	req, err := http.NewRequest("GET", "https://functions.api.ticos-systems.cloud/api/gates/counter?organizationUnitIds=30208", nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header = http.Header{
+		"Abp-TenantId": []string{"69"},
+		"Abp.TenantId": []string{"69"},
+		"Origin":       []string{"https://www.swm.de"},
+		"Referer":      []string{"https://www.swm.de/"},
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	println(res)
 }
