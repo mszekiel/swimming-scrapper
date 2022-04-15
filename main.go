911fc5353a087585801a17a7d9de8d6e441bc71a
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2022-04-15 22:48:33 +0200

    add saunas to fetch

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ type Pool struct {
 
 func getPoolData() []Pool {
 	client := http.Client{}
-	req, err := http.NewRequest("GET", "https://functions.api.ticos-systems.cloud/api/gates/counter?organizationUnitIds=30208&organizationUnitIds=30194&organizationUnitIds=30195&organizationUnitIds=30190&organizationUnitIds=129&organizationUnitIds=30208&organizationUnitIds=30197&organizationUnitIds=30184&organizationUnitIds=30182&organizationUnitIds=30187&organizationUnitIds=30199", nil)
+	req, err := http.NewRequest("GET", "https://functions.api.ticos-systems.cloud/api/gates/counter?organizationUnitIds=30208&organizationUnitIds=30194&organizationUnitIds=30195&organizationUnitIds=30190&organizationUnitIds=129&organizationUnitIds=30208&organizationUnitIds=30197&organizationUnitIds=30184&organizationUnitIds=30182&organizationUnitIds=30187&organizationUnitIds=30199&organizationUnitIds=30204&organizationUnitIds=30207&organizationUnitIds=30188&organizationUnitIds=30200&organizationUnitIds=30206&organizationUnitIds=30185&organizationUnitIds=30203&organizationUnitIds=30191&organizationUnitIds=30201", nil)
 
 	if err != nil {
 		log.Fatalln(err)
