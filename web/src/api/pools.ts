4cf716620cf3e6d42f154bec72afee33311b3998
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2022-06-08 11:55:16 +0200

    fix: api url

--- web/src/api/pools.ts
+++ web/src/api/pools.ts
@@ -1,5 +1,7 @@
+const BASE_URL = import.meta.env.VITE_API_URL || "";
+
 export const getPools = async () => {
-  const response = await fetch("/api/pools");
+  const response = await fetch(`${BASE_URL}/api/pools`);
 
   return await response.json();
 };
