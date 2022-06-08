4405dfb0cdcc2900c2147251c348592ecea3540d
Miłosz Szekiel <12242002+mszekiel@users.noreply.github.com>
2022-06-08 11:57:34 +0200

    fix: data type for fetch

--- web/src/api/pools.ts
+++ web/src/api/pools.ts
@@ -1,7 +1,18 @@
 const BASE_URL = import.meta.env.VITE_API_URL || "";
 
+interface Pool {
+  identificator: number;
+  location: { lat: number; lon: number };
+  name: string;
+}
+
+interface CountedResponse<T> {
+  count: number;
+  data: T[];
+}
+
 export const getPools = async () => {
   const response = await fetch(`${BASE_URL}/api/pools`);
 
-  return await response.json();
+  return (await response.json()) as CountedResponse<Pool>;
 };
